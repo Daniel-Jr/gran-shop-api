a90b696489c84bd48ec1d6fd245223b842581c91
Daniel Debrande Junior <daniel.debrande.jr@gmail.com>
2025-05-22 21:39:42 -0300

    Add Swagger

--- GranShopAPI/Program.cs
+++ GranShopAPI/Program.cs
@@ -6,12 +6,32 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "GranShop",
+            Version = "v1",
+            Description = "Documentação da API",
+
+        });
+    }
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(
+        c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "GranShop API v1");
+        }
+    );
 }
 
 app.UseHttpsRedirection();
